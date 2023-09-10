245b0f0c1bab28dcd39c932815434062855ffc6a
svaught598 <svaught@DESKTOP-8G3GMGU>
2023-09-09 21:34:12 -0600

    hook up exp part to mapper and label changes
    hook up exp part to mapper and label changes

--- src/lib/widgets/pokemon/TypeTag.vue
+++ src/lib/widgets/pokemon/TypeTag.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { getTypeColor } from '../../../util/pokemon';
-
-const { type } = defineProps({
+defineProps({
   type: { type: String, required: true },
 });
-
-const style = computed(() => `background-color: ${getTypeColor(type)}`);
 </script>
 
 <template>
-  <div :style="style" class="py-1 px-5 font-primary rounded border border-black">
-    {{ type }}
-  </div>
+  <img width="112" :src="`../../../../data/pokemon-gen-2/images/type-icons/${type.toLocaleLowerCase()}.png`" />
 </template>
